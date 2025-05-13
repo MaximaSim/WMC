a5bc34ef54ea67b1ea3c6f79dce481c4b59cac57
MaximaSim <sim240770@spengergasse.at>
2025-05-13 22:32:00 +0200

    Debugger test

--- 01_Primzahlen/2025-02__Server/main.ts
+++ 01_Primzahlen/2025-02__Server/main.ts
@@ -6,7 +6,7 @@ const app = new Hono();
 // serve index.html
 app.get("/", serveStatic({ path: "./static/index.html" }));
 
-// serve json
+/* serve json
 const arr = [1, 2, 3, 4, 5];
 const obj = { name: "John", age: 30, city: "New York" };
 app.get("/obj", (c) => c.json(obj));
@@ -19,9 +19,33 @@ app.get(
       new Array(10).fill().map((_) => Math.round(Math.random() * 50))
         .toSorted(),
     ),
-);
+);*/
+
+app.get("/checkprim", (c) => {
+  const num = Number.parseInt(c.req.query("num") || "0");
+  if(isNaN(num)) {
+    return c.json({ error: "ZAHL EINGEBEN HAB ICH GESAGT !" });
+  }
+  return c.json({result: isPrim(num)});
+});
+
+function isPrim(num: number) {
+  if (num <= 1) return false;
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
+}
 
 // serve all other static files
 app.get("*", serveStatic({ root: "./static" }));
 
-Deno.serve(app.fetch);
+const port = 8000;
+
+Deno.serve({
+  
+  port, 
+  onListen: () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  },
+}, app.fetch);
